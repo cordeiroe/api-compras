f5ee819307a02c3b94053ecc710961a3cb57a956
Émerson Marques <emersoncmarques@outlook.com>
2022-09-06 01:21:36 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/emerson/course/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.emerson.course.repositories;
+
+import com.emerson.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
